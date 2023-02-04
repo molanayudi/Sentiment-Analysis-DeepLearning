444a0aeeec1becee24c5e9b514e2987d6f609a42
molanayudi <54979189+molanayudi@users.noreply.github.com>
2023-02-04 12:27:12 +0700

    init pgit add .

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("hello world")
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
+from routes import analytics, cleansing
+
+app = FastAPI()
+
+@app.get("/version")
+async def index():
+    return JSONResponse(
+        content = {
+            "ok": True,
+            "code": 200,
+            "data": {"version": "1.0.0"},
+            "messege": "Success"
+        }
+    )
+    
+
+
+app.include_router(analytics.router, tags=["Analytics"])
+app.include_router(cleansing.router, tags=["Cleansing"])
---
+++ routes/analytics.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+from services.analytics import AnalyticServices
+
+router = APIRouter()
+
+@router.get("/sentiment")
+async def sentiment_analytics(
+    text:str
+):
+    result = await AnalyticServices().get_sentiment_analytics(text= text)
+    return result
+
+
+
